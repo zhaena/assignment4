978fc36f178f81f45aac3f1fc08b891874fdcb00
zhaena <200103331>
2023-05-13 15:05:03 +0500

    DONE!

--- src/test.py
+++ src/test.py
@@ -1,11 +1,6 @@
-import unittest
+import pytest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_addition(self):
-        result = 2 + 2
-        self.assertEqual(result, 4)
-
-
-if __name__ == '__test__':
-    unittest.main()
+def test_addition():
+    result = 2 + 2
+    assert result == 4
